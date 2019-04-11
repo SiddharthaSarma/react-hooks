7efd6214dd52049c98f1ed3f87e982e88e2ec84d
siddharthasarma <sidduinevitable@gmail.com>
2019-04-11 12:33:50 +0530

    update code to get the prev state

--- src/Counter.js
+++ src/Counter.js
@@ -2,13 +2,21 @@ import React, { useState } from 'react';
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(prevState => prevState + 1);
+  };
+
+  const decrementCount = () => {
+    setCount(prevState => prevState - 1);
+  };
   return (
     <div>
       <span>{count}</span>
-      <button className="pure-button" onClick={() => setCount(count + 1)}>
+      <button className="pure-button" onClick={incrementCount}>
         +
       </button>
-      <button className="pure-button" onClick={() => setCount(count - 1)}>
+      <button className="pure-button" onClick={decrementCount}>
         -
       </button>
     </div>
